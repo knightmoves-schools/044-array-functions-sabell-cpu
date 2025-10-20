92404a183d824f79801d4b375975c191b5336676
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-10-20 18:20:21 +0000

    array functions

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 var automobiles = ['car', 'truck', 'motorcycle'];
-
 automobiles.push('airplane','skateboard');
-document.getElementById("push").innerHTML = automobiles;
-
 automobiles.unshift('helicopter','bike');
-document.getElementById("unshift").innerHTML = automobiles;
-
 var automobilesCopy = `Automobiles: ${automobiles}`;
-
 automobiles.pop();
-document.getElementById("pop").innerHTML = automobiles;
-
 automobiles.shift();
-document.getElementById("shift").innerHTML = automobiles;
-
 document.getElementById("result").innerHTML = automobiles;
